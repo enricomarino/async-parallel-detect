96dbbd9a3b3a82a2c80d863a7f1fb7f940756264
Enrico Marino <enrico.marino@email.com>
2013-01-13 08:44:18 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+module.exports = parallel_detect;
+
+/*
+ * parallel_detect
+ * Detect items of 'array' that pass 'iterator' in parallel
+ * and call 'callback' when done
+ *
+ * @param {Array} array array
+ * @param {Function} iterator iterator
+ * @param {Function} callback callback
+ * @api public
+ */
+
+function parallel_detect(array, iterator, callback) {
+  var completed = 0;
+  var len = array.length; 
+  var i;
+
+  function complete(err, result) {
+    if (err) {
+      callback(err);
+      callback = function () {};
+      return;
+    }
+    if (result) {
+      callback(err, result);
+      callback = function () {};
+      return;
+    }
+    completed += 1;
+    if (completed === len) {
+      callback(err, result);
+      return;
+    }
+  }
+
+  for (i = 0; i < len; i += 1) {
+    iterator(array[i], complete);
+  }
+}
